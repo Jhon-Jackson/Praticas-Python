858e7735ec48b07cf3c9603fafa870c459f215ff
Jhon Jackson S S <radicalug@gmail.com>
2025-06-17 12:12:43 -0300

    01

--- praticar.py
+++
@@ -1,9 +0,0 @@
-itens = {}
-produtos = []
-
-itens["nome"] = str(input("seu nome: "))
-itens["idade"] = int(input('Sua idade: '))
-produtos.append(itens.copy())
-print(produtos)
-print(produtos[0]['nome'])
-print(produtos[0]['idade'])
