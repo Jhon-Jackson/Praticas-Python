b1be151ffb2c655c0a5955763a093cc672a123b7
Jhon Jackson S S <radicalug@gmail.com>
2025-07-14 22:21:26 -0300

    06

--- Praticando/ex002.py
+++ Praticando/ex002.py
@@ -23,12 +23,31 @@ while True:
     print("para Finalizar Digite Sair")
     nota = input("Escreva uma nota: ")
 
-    with open(Arquivo, "a") as file:
+    with open(Arquivo, "a+", encoding= 'utf-8') as file:
         file.write(nota + "!\n" )
 
     if nota == "sair":
+        linhas = list()
+        with open(Arquivo, 'r') as file:
+            for linha in file:
+                linhas.append(linha)
+            linhas.pop(-1)
         break
 
+while True:
+    with open(Arquivo, 'w') as file:
+        file.writelines(linhas)
+
+
+    with open(Arquivo, "r") as file :
+        for idx, linha in enumerate(file):
+            print(f"{idx + 1}. {linha}")
+
+    excluir_linha = int(input("Digito o numero para excluir a linha (0  para sair): ")) - 1
+    linhas.pop(excluir_linha)
+    if excluir_linha == -1 :
+        break
+print('Arquivo salvo com as informações: ')
 with open(Arquivo, "r") as file :
     for idx, linha in enumerate(file):
         print(f"{idx + 1}. {linha}")
